340d828120df8f3a7d7947f0d09fec21c10a9400
Yaroslav Semenov <yaryk2013@gmail.com>
2022-01-15 17:23:13 +0200

    completed task

--- src/index.js
+++ src/index.js
@@ -13,28 +13,52 @@ const refs = {
 refs.input.addEventListener('input', debounce(OnInput, DEBOUNCE_DELAY));
 
 function OnInput(e) {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+
   const country = e.target.value.trim();
   if (!country) {
     return;
   }
 
-  console.log(country);
-
   fetchCountries(country)
-    .then(ref => console.log(ref))
-    .catch(error => console.log(error));
+    .then(ref => {
+      if (ref.length === 1) {
+        countryInfoMarkup(ref);
+      } else if (2 <= ref.length && ref.length <= 10) {
+        countryListMarkup(ref);
+      } else {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+      }
+    })
+    .catch(() => Notify.failure('Oops, there is no country with that name'));
 }
 
 function countryInfoMarkup(country) {
   country.forEach(({ name, capital, population, flags, languages }) => {
     refs.countryInfo.innerHTML = `
-      <div>
+      <h1>
           <img src='${flags.svg}' alt='flags' style='height:20px'>
           ${name.official}
-      </div>
-      <div>Capital: ${capital.join(', ')}</div>
-      <div>Population: ${population}</div>
-      <div>Languages: ${Object.values(languages).join(', ')}</div>
+      </h1>
+      <p><span class='subtitle'>Capital:</span> ${capital.join(', ')}</p>
+      <p><span class='subtitle'>Population:</span> ${population}</p>
+      <p><span class='subtitle'>Languages:</span> ${Object.values(languages).join(', ')}</p>
       `;
   });
+}
+
+function countryListMarkup(countries) {
+  const markup = countries
+    .map(({ name, flags }) => {
+      return `
+    <li>
+      <img src='${flags.svg}' alt='flags' style='height:20px'>
+      ${name.official}
+    </li>
+    `;
+    })
+    .join('');
+
+  refs.countryList.insertAdjacentHTML('beforeend', markup);
 }
